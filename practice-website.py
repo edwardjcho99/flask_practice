8e65bd05c1f1c5efd5c4b03625395447fa0079f1
Edward Cho <edwardjcho99@gmail.com>
2020-05-27 18:15:00 -0700

    added html templates and styling

--- practice-website.py
+++ practice-website.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author':'edward cho',
+        'title':'Blog Post 1',
+        'content':'first post content',
+        'date_posted':'April 20, 2020'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog Post 2',
+        'content':'second post content',
+        'date_posted':'April 22, 2020'
+    }
+]
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
