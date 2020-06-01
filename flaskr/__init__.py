bf3cd0ccfec7c8d3264c3db650a6099e9d3ca4e7
Edward Cho <edwardjcho99@gmail.com>
2020-05-31 23:16:38 -0700

    created authentication blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 # factory for creating the webapp
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, hfgfg_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -29,5 +29,8 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
